4f730e20ce879edb8a6bfb61f468fe61fc7ec3a7
Matt Mills <mattmills1023@gmail.com>
2018-12-30 12:20:37 -0600

    Update home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../styles/Home.scss';
+
 const Home = () => {
   return (
-    <div>
-      Home
-      <p>
-        <Link to="/canvas">Try It</Link>
+    <div className="home">
+      <h1>ABSTRACTIONS</h1>
+      <p className="description-text">
+        This project is currently a work in progress.
+        <br />
+        Please ignore any weirdness or non-functionality. 👍
       </p>
+      <Link className="cta-button" to="/canvas">
+        Try It
+      </Link>
+      <footer className="footer">
+        A creative coding project by{' '}
+        <a href="https://mattmills.xyz">Matt Mills</a>.
+      </footer>
     </div>
   );
 };
