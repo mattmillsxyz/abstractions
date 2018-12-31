91c16e6b8978adb9c5b3162a46f048f34130c7f7
Matt Mills <mattmills1023@gmail.com>
2018-12-30 22:29:56 -0600

    Adding randomize feature.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { find } from 'lodash';
 
 import '../styles/Menu.scss';
 import EditMenu from './edit/EditMenu';
+import { selectDesign, selectPalette, selectDirection } from '../actions';
 
 class Menu extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      showEditMenu: false
+      showEditMenu: false,
     };
+  }
+
+  randomize() {
+    const { designs, palettes, directions } = this.props;
+    const randomDesign = designs[Math.floor(Math.random() * designs.length)];
+    this.props.selectDesign(randomDesign);
+
+    const randomPaletteNum =
+      randomDesign.palettes[
+        Math.floor(Math.random() * randomDesign.palettes.length)
+      ];
+    const randomPalette = find(palettes, palette => {
+      if (palette.name === randomPaletteNum) {
+        return palette;
+      }
+    });
+    this.props.selectPalette(randomPalette);
+
+    const randomDirection =
+      directions[Math.floor(Math.random() * directions.length)];
+    this.props.selectDirection(randomDirection);
   }
 
   toggle() {
     this.setState({
-      showEditMenu: !this.state.showEditMenu
+      showEditMenu: !this.state.showEditMenu,
     });
   }
 
@@ -25,7 +49,9 @@ class Menu extends React.Component {
         <button onClick={() => this.toggle()} className="edit-button">
           EDIT
         </button>
-        <button className="random-button">RANDOM</button>
+        <button className="random-button" onClick={() => this.randomize()}>
+          RANDOM
+        </button>
         <button className="share-button" disabled>
           SHARE
         </button>
@@ -34,4 +60,15 @@ class Menu extends React.Component {
   }
 }
 
-export default Menu;
+const mapStateToProps = state => {
+  return {
+    designs: state.designs,
+    palettes: state.palettes,
+    directions: state.directions,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { selectDesign, selectPalette, selectDirection }
+)(Menu);
